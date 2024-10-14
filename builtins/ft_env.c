445f63fff58eb3671bc1465ac168fad768193bd6
Boussaden Taha <117488861+ThePhoenix77@users.noreply.github.com>
2024-10-14 18:45:35 +0100

    ft_env.c

--- builtins/ft_env.c
+++ builtins/ft_env.c
@@ -1,29 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_env.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: eaboudi <eaboudi@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/01 09:32:45 by eaboudi           #+#    #+#             */
-/*   Updated: 2024/10/01 09:32:46 by eaboudi          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
+/**
+ * Prints the current environment variables to standard output.
+ * Each variable is printed on a new line, but only those 
+ * that contain an '=' character are included.
+ *
+ * @param global Pointer to the global state, which contains the 
+ *               linked list of environment variables (env_list).
+ */
 void	ft_env(t_global *global)
 {
-	t_env_list	*current;
+    t_env_list	*current; // Pointer to iterate through the environment list.
 
-	current = global->env_list;
-	while (current)
-	{
-		if (ft_strchr(current->content, '='))
-		{
-			ft_putstr_fd(current->content, STDOUT_FILENO);
-			ft_putstr_fd("\n", STDOUT_FILENO);
-		}
-		current = current->next;
-	}
+    current = global->env_list; // Start from the head of the environment list.
+    while (current) // Iterate through the linked list.
+    {
+        // Check if the current node's content contains an '=' character,
+        // indicating it is a valid environment variable.
+        if (ft_strchr(current->content, '='))
+        {
+            // Print the environment variable followed by a newline.
+            ft_putstr_fd(current->content, STDOUT_FILENO);
+            ft_putstr_fd("\n", STDOUT_FILENO);
+        }
+        current = current->next; // Move to the next environment variable.
+    }
 }
