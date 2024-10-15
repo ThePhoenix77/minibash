290cf090923ef1859e4313e718b4308f79ea4f9c
Boussaden Taha <117488861+ThePhoenix77@users.noreply.github.com>
2024-10-15 15:05:57 +0100

    exc_list_utils.c

--- excution/exc_list_utils.c
+++ excution/exc_list_utils.c
@@ -1,66 +1,58 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   exc_list_utils.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: eaboudi <eaboudi@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/06 18:49:25 by eaboudi           #+#    #+#             */
-/*   Updated: 2024/10/04 11:19:54 by eaboudi          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
+// Create a new execution list node
 t_exc_list	*new_exc_spot(t_global *global, t_type_node type)
 {
 	t_exc_list	*new;
 
-	new = (t_exc_list *)malloc(sizeof(t_exc_list));
+	new = (t_exc_list *)malloc(sizeof(t_exc_list)); // Allocate memory for a new execution node
 	if (!new)
-		malloc_failed(global);
-	new->type = type;
-	new->redir = NULL;
-	new->next = NULL;
-	new->fd.in = -1;
-	new->fd.out = -1;
-	new->cmd_args = NULL;
-	return (new);
+		malloc_failed(global); // Handle memory allocation failure
+	new->type = type; // Set the type of the execution node
+	new->redir = NULL; // Initialize redirection to NULL
+	new->next = NULL; // Initialize the next pointer to NULL
+	new->fd.in = -1; // Initialize input file descriptor
+	new->fd.out = -1; // Initialize output file descriptor
+	new->cmd_args = NULL; // Initialize command arguments to NULL
+	return (new); // Return the newly created execution node
 }
 
+// Get the last node in the execution list
 t_exc_list	*last_exc_node(t_exc_list *head)
 {
-	if (!head)
+	if (!head) // If the list is empty, return NULL
 		return (NULL);
-	while (head->next)
+	while (head->next) // Traverse to the last node
 		head = head->next;
-	return (head);
+	return (head); // Return the last node
 }
 
+// Add a new execution node to the end of the list
 void	add_back_exc(t_exc_list **head, t_exc_list *new)
 {
 	t_exc_list	*last_node;
 
-	if (!new && !(*head))
+	if (!new && !(*head)) // If both new and head are NULL, do nothing
 		return ;
-	if (!(*head))
+	if (!(*head)) // If the list is empty, set head to new
 		*head = new;
 	else
 	{
-		last_node = last_exc_node((*head));
-		last_node->next = new;
+		last_node = last_exc_node((*head)); // Find the last node in the list
+		last_node->next = new; // Link the new node at the end
 	}
 }
 
+// Count the number of nodes in the execution list
 int	exc_list_size(t_exc_list *exc)
 {
 	int	i;
 
-	i = 0;
-	while (exc)
+	i = 0; // Initialize the counter to 0
+	while (exc) // Traverse the list
 	{
-		i++;
-		exc = exc->next;
+		i++; // Increment the counter for each node
+		exc = exc->next; // Move to the next node
 	}
-	return (i);
+	return (i); // Return the total count of nodes
 }
