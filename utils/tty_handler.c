409e6eae4d74d2ad2b44b99708f89fe3bd80c420
Boussaden Taha <117488861+ThePhoenix77@users.noreply.github.com>
2024-10-15 14:59:41 +0100

    Update tty_handler.c

--- utils/tty_handler.c
+++ utils/tty_handler.c
@@ -1,42 +1,33 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   tty_handler.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: eaboudi <eaboudi@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/04 20:15:25 by eaboudi           #+#    #+#             */
-/*   Updated: 2024/10/05 16:51:42 by eaboudi          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
+// Save the current terminal attributes to the global structure
 void	save_tty(t_global *global)
 {
-	if (tcgetattr(STDIN_FILENO, &global->t_termios) == -1)
+	if (tcgetattr(STDIN_FILENO, &global->t_termios) == -1) // Get terminal attributes
 	{
-		perror("minishell: tcgetattr");
-		malloc_failed(global);
+		perror("minishell: tcgetattr"); // Print error if unsuccessful
+		malloc_failed(global); // Handle memory failure
 	}
 }
 
+// Restore the saved terminal attributes from the global structure
 void	reset_tty(t_global *global)
 {
-	if (tcsetattr(STDIN_FILENO, TCSANOW, &global->t_termios) == -1)
+	if (tcsetattr(STDIN_FILENO, TCSANOW, &global->t_termios) == -1) // Set terminal attributes
 	{
-		perror("minishell: tcsetattr");
-		malloc_failed(global);
+		perror("minishell: tcsetattr"); // Print error if unsuccessful
+		malloc_failed(global); // Handle memory failure
 	}
 }
 
+// Handle memory allocation failure
 void	malloc_failed(t_global *global)
 {
-	free_memory(global);
+	free_memory(global); // Free allocated memory in the global structure
 	if (global->content)
-		free(global->content);
+		free(global->content); // Free content if allocated
 	if (global->env_list)
-		free_env_list(&global->env_list);
-	global->exit_status = 1;
-	exit(1);
+		free_env_list(&global->env_list); // Free the environment list if allocated
+	global->exit_status = 1; // Set exit status to 1 to indicate failure
+	exit(1); // Exit the program
 }
