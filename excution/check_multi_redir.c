e948d7b918d04dea2c3a74ee7bf61c799fe40f72
Boussaden Taha <117488861+ThePhoenix77@users.noreply.github.com>
2024-10-15 15:03:02 +0100

    check_multi_redir.c

--- excution/check_multi_redir.c
+++ excution/check_multi_redir.c
@@ -1,98 +1,89 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   check_multi_redir.c                                :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: eaboudi <eaboudi@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/05 14:53:51 by eaboudi           #+#    #+#             */
-/*   Updated: 2024/10/05 15:35:18 by eaboudi          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
+// Create a new redirection node by copying an existing one
 t_redir	*new_coipe_list(t_redir *node)
 {
 	t_redir	*new;
 
-	new = (t_redir *)malloc(sizeof(t_redir));
+	new = (t_redir *)malloc(sizeof(t_redir)); // Allocate memory for new redirection node
 	if (!new)
-		return (NULL);
-	new->file_name = ft_strdup(node->file_name);
+		return (NULL); // Return NULL on failure
+	new->file_name = ft_strdup(node->file_name); // Duplicate the file name
 	if (!new->file_name)
 	{
-		free(new);
+		free(new); // Free allocated memory if strdup fails
 		return (NULL);
 	}
-	new->file_type = node->file_type;
-	new->type = node->type;
-	new->next = NULL;
+	new->file_type = node->file_type; // Copy file type
+	new->type = node->type; // Copy type
+	new->next = NULL; // Initialize next pointer to NULL
 	return (new);
 }
 
+// Open files based on the redirection type and handle errors
 bool	open_redir_files(t_redir *node)
 {
 	int	fd;
 
 	if (node->type == REDIR_OUT)
 	{
-		fd = open(node->file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		fd = open(node->file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644); // Open file for output
 		if (fd < 0)
-			return (perror("minishell: open"), FAILURE);
+			return (perror("minishell: open"), FAILURE); // Handle open error
 	}
 	else if (node->type == DOUBLE_QUOTE)
 	{
-		fd = open(node->file_name, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		fd = open(node->file_name, O_WRONLY | O_CREAT | O_APPEND, 0644); // Open file for appending
 		if (fd < 0)
 			return (perror("minishell: open"), FAILURE);
 	}
 	else if (node->type == REDIR_IN)
 	{
-		fd = open(node->file_name, O_RDONLY, 0644);
+		fd = open(node->file_name, O_RDONLY, 0644); // Open file for input
 		if (fd < 0)
 			return (perror("minishell: open"), FAILURE);
 	}
 	if (node->file_type == HERE_DOC_FILE)
-		return (unlink(node->file_name), SUCCESS);
+		return (unlink(node->file_name), SUCCESS); // Unlink here-document files
 	else if (node->type == ERROR_DIS)
-		return (SUCCESS);
-	return (close(fd), SUCCESS);
+		return (SUCCESS); // Handle error-disallowing types
+	return (close(fd), SUCCESS); // Close file descriptor and return success
 }
 
+// Add a copied redirection node to the global state
 void	add_copie_list(t_global *global, t_redir **new_head, t_redir *old)
 {
 	t_redir	*new;
 
-	new = NULL;
-	new = new_coipe_list(old);
+	new = new_coipe_list(old); // Create a new copied node
 	if (!new)
-		(free_redir_list(new_head), malloc_failed(global));
-	add_back_redir(new_head, new);
+		(free_redir_list(new_head), malloc_failed(global)); // Handle malloc failure
+	add_back_redir(new_head, new); // Add new node to the linked list
 }
 
+// Check for double redirection and manage them accordingly
 bool	check_doubel_redir(t_global *global, t_redir **redir_list)
 {
 	t_redir	*tmp;
 	t_redir	*new_head;
 
-	new_head = NULL;
-	tmp = *redir_list;
+	new_head = NULL; // Initialize new head for the new redirection list
+	tmp = *redir_list; // Start from the original redirection list
 	while (tmp)
 	{
-		while (tmp->next && (tmp->type == tmp->next->type)
+		while (tmp->next && (tmp->type == tmp->next->type) // Check for consecutive redirections
 			&& (tmp->file_type != ERROR_DIS))
 		{
-			if (open_redir_files(tmp) == FAILURE)
-				return (free_redir_list(&new_head), FAILURE);
-			tmp = tmp->next;
+			if (open_redir_files(tmp) == FAILURE) // Open the file for the current node
+				return (free_redir_list(&new_head), FAILURE); // Handle failure
+			tmp = tmp->next; // Move to the next node
 		}
-		add_copie_list(global, &new_head, tmp);
-		if (tmp->file_type == ERROR_DIS)
+		add_copie_list(global, &new_head, tmp); // Add the current node to the new list
+		if (tmp->file_type == ERROR_DIS) // Break on error-disallowing types
 			break ;
-		tmp = tmp->next;
+		tmp = tmp->next; // Move to the next node
 	}
-	free_redir_list(redir_list);
-	*redir_list = new_head;
-	return (SUCCESS);
+	free_redir_list(redir_list); // Free the original list
+	*redir_list = new_head; // Update the original pointer to the new list
+	return (SUCCESS); // Return success
 }
