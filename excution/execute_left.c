28633f9f8734b2a56341c6608d423eed35711cbf
Boussaden Taha <117488861+ThePhoenix77@users.noreply.github.com>
2024-10-15 15:09:12 +0100

    execute_left.c

--- excution/execute_left.c
+++ excution/execute_left.c
@@ -1,42 +1,36 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   execute_left.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: eaboudi <eaboudi@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/27 10:49:16 by eaboudi           #+#    #+#             */
-/*   Updated: 2024/10/04 11:33:52 by eaboudi          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
+// Execute the left child of a command in the syntax tree
 int	execute_left(t_global *global, t_tree *root, int *pipe)
 {
+	// Check if there are redirection operations for the left child
 	if (root->left->redir)
 	{
+		// Handle the redirection and update the file descriptors
 		if (handle_redirection(root->left->redir, &root->left->fds) == SUCCESS)
 		{
+			// If there is an input file descriptor, duplicate it to stdin
 			if (root->left->fds.in != -1)
 			{
 				if (dup2(root->left->fds.in, 0) < 0)
 					return (perror("minishell: dup2:"), -1);
-				close(root->left->fds.in);
+				close(root->left->fds.in); // Close the original fd after duplication
 			}
+			// If there is an output file descriptor, duplicate it to stdout
 			if (root->left->fds.out != -1)
 			{
 				if (dup2(root->left->fds.out, 1) < 0)
 					return (perror("minishell: dup2:"), -1);
-				(close(root->left->fds.out), execute_tree(global, root->left));
-				return (close_pipe(pipe), SUCCESS);
+				(close(root->left->fds.out), execute_tree(global, root->left)); // Execute the left child
+				return (close_pipe(pipe), SUCCESS); // Close the pipe and return success
 			}
 		}
 	}
+	// If no redirection, duplicate the write end of the pipe to stdout
 	if (dup2(pipe[1], 1) < 0)
 		return (perror("minishell: dup2:"), -1);
-	close(pipe[1]);
-	close(pipe[0]);
-	execute_tree(global, root->left);
-	return (SUCCESS);
+	close(pipe[1]); // Close the write end of the pipe
+	close(pipe[0]); // Close the read end of the pipe
+	execute_tree(global, root->left); // Execute the left child
+	return (SUCCESS); // Return success
 }
